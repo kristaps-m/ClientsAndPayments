932ba11f02e572d9f72dde0113307c9c63154822
Kristaps-M <kristapsmitins@gmail.com>
2025-07-16 11:09:27 +0300

    Add models install nugget

---
+++ src/api/ClientsAndPaymentsApi/ClientsAndPayments.Core/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientsAndPayments.Core.Models
+{
+    public class Client:Entity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public DateTime RegistredAt {  get; set; }
+    }
+}
---
+++ src/api/ClientsAndPaymentsApi/ClientsAndPayments.Core/Models/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientsAndPayments.Core.Models
+{
+    public class Payment: Entity
+    {
+        [Required]
+        public int ClientId { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public string Currency { get; set; } = string.Empty;
+        [Required]
+        public DateTime PaidAt { get; set; }
+        [StringLength(120)]
+        public string Note { get; set; } = string.Empty;
+    }
+}
