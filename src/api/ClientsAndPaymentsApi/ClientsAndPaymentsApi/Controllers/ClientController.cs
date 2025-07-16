15c0ced2b4939ee9fff61b34996790050352ce66
Kristaps-M <kristapsmitins@gmail.com>
2025-07-16 13:10:41 +0300

    add 2 contollers to get all T[]

---
+++ src/api/ClientsAndPaymentsApi/ClientsAndPaymentsApi/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using ClientsAndPayments.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientsAndPaymentsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [Route("all")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var entities = _clientService.GetAll();
+            return Ok(entities);
+        }
+    }
+}
---
+++ src/api/ClientsAndPaymentsApi/ClientsAndPaymentsApi/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using ClientsAndPayments.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientsAndPaymentsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Route("all")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var entities = _paymentService.GetAll();
+            return Ok(entities);
+        }
+    }
+}
